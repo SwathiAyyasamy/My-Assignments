ff691f52d8d6076d38fe4edeecbcf03671f2f8e1
SwathiAyyasamy <134796730+SwathiAyyasamy@users.noreply.github.com>
2023-05-27 20:05:54 -0700

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -7,11 +7,11 @@ public class IsPrime {
   int n= 29;
  for(int i=2; i<=n-1; i++) {
 if (n%i ==0) {
-	System.out.println(n +"Is a prime number");
+	System.out.println(n +"Is a not prime number");
 	break;
 }
 else if (n%i!=0){
-	System.out.println(n +"Is not aprime number");
+	System.out.println(n +"Is a prime number");
 	break;
 }
 
